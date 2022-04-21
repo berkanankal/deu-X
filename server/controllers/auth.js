577221f627d528eab6f80bbc4ce257664e3ad87d
berkanankal <berkan.ankal@outlook.com>
2022-04-21 16:16:26 +0300

    Express-async-handler

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,18 +1,15 @@
 const User = require("../models/User");
+const asyncHandler = require("express-async-handler");
 
-const register = async (req, res, next) => {
-  try {
-    const data = req.body;
+const register = asyncHandler(async (req, res, next) => {
+  const data = req.body;
 
-    const user = await User.create(data);
+  const user = await User.create(data);
 
-    res.status(201).json({
-      success: "true",
-      data: user,
-    });
-  } catch (err) {
-    return next(err);
-  }
-};
+  res.status(201).json({
+    success: "true",
+    data: user,
+  });
+});
 
 module.exports = { register };

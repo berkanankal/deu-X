1796125925d9570864bf7c3a5236dc1450179f3e
berkanankal <berkan.ankal@outlook.com>
2022-04-21 12:59:51 +0300

    Controller functions created

--- server/routes/questions.js
+++ server/routes/questions.js
@@ -1,11 +1,8 @@
 const express = require("express");
+const { getAllQuestions } = require("../controllers/questions");
+
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  res.send("Questions home");
-});
-router.get("/delete", (req, res, next) => {
-  res.send("Questions delete");
-});
+router.get("/", getAllQuestions);
 
 module.exports = router;
